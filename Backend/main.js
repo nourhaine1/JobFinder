048405b1c2a73efdd1f52b14311dfbe7f0345f50
nourhaine1 <ayadinourhaine@gmail.com>
2023-12-20 21:01:43 +0100

    add express and install nodemon

--- Backend/main.js
+++ Backend/main.js
@@ -1,1 +1,9 @@
+const express= require('express');
+const app =express();
+app.listen(3000,()=>{
+console.log('listening on port 3000');
+})
+app.get("/",(req,res)=>{
+res.send ("Hello, world!");
+});
 console.log("Aaselemmaaa");
