b5eaaadebe3f0e233aa4af120755c616df6a45a9
nourhaine1 <ayadinourhaine@gmail.com>
2023-12-21 21:25:54 +0100

    crud user

--- Backend/main.js
+++ Backend/main.js
@@ -1,9 +1,23 @@
-const express= require('express');
-const app =express();
-app.listen(3000,()=>{
-console.log('listening on port 3000');
-})
-app.get("/",(req,res)=>{
-res.send ("Hello, world!");
+const express = require('express');
+const mongoose = require('mongoose');
+const UserController = require('./Controllers/UserController');
+
+const app = express();
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/JobFinder')
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.log('Error connecting to MongoDB'));
+
+// Define routes using controller functions
+app.post('/user', UserController.createUser);
+app.get('/Users', UserController.getAllUsers);
+app.get('/Users/:id', UserController.getUserById);
+app.put('/Users/:id', UserController.updateUser);
+app.delete('/Users/:id', UserController.deleteUser);
+
+// Start server
+app.listen(3000, () => {
+    console.log(`Server is running on port 3000`);
 });
-console.log("Aaselemmaaa");
