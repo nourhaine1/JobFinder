875190fb30dd6958bfbda8ccdac1dc847a0f45c5
hayder <haidermissaoui10@gmail.com>
2023-12-22 14:13:35 +0100

    Added server.js file

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+    // Using Node.js `require()`
+const mongoose = require('mongoose');
+
+/*app.listen(9002, function check(error) {
+    if (error)
+        console.log("Error ...!");
+    else
+        console.log("Started ...!");
+});*/
+
+app.listen(9992, function check(err) {
+    if (error)
+        console.log("error")
+    else
+        console.log("started")
+});
