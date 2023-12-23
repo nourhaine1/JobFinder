3efc2923b3fb77bda8cf19bd9c7a2e2177a62c36
hayder <haidermissaoui10@gmail.com>
2023-12-23 17:15:21 +0100

    Added routes.js file

--- Backend/routes/routes.js
+++ Backend/routes/routes.js
@@ -11,18 +11,38 @@ router.post('register', async(req, res) => {
     let password = req.body.password
     let name = raq.body.name
 
-    const user = new User({
-        name: name,
-        email: email,
-        password: password
-    })
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
 
-    const result = await user.save()
-    res.json({
-        //id:result._id
-        user: result
+    const record = await User.findOne({ email: email })
+    if (record) {
+        return res.status(400).send({
+            message: "Email is already registered"
+        })
+    } else {
 
-    })
+        const user = new User({
+            name: name,
+            email: email,
+            //password: password
+            password: hashedPassword
+        })
+
+        const result = await user.save()
+
+        //JWT Token
+        const { _id } = await result.toJSON()
+        res.cookie("jwt", token, {
+            httpOnly: true,
+            maxAge: 24 * 60 * 60 * 1000
+        })
+        res.json({
+            //id:result._id
+            //user: result
+            message: "success"
+
+        })
+    }
 })
 
 router.post("/login", async(req, res) => {
