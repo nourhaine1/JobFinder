95e930a485ea3fd58602c419100fe476382d42dc
nourhaine1 <ayadinourhaine@gmail.com>
2023-12-27 22:46:46 +0100

    list companies

--- frontend/src/app/company/company-list/company-list.component.ts
+++ frontend/src/app/company/company-list/company-list.component.ts
@@ -9,14 +9,39 @@ import { Subscription } from 'rxjs';
   styleUrls: ['./company-list.component.css']
 })
 export class CompanyListComponent implements OnInit {
-  companies? : Company[];
+  companies: Company[]=[];
   subscription? : Subscription;
+  companiesFound?:Number
+  currentPage = 1;
+  companiesPerPage = 5;
 
   constructor(private service : CompanyServiceService){}
-ngOnInit(): void {
-  this.service.getCompanies().subscribe(
-    companies => this.companies = companies
-  );
 
+getCurrentPageCompanies(): any[] {
+  const startIndex = (this.currentPage - 1) * this.companiesPerPage;
+  const endIndex = startIndex + this.companiesPerPage;
+  return this.companies.slice(startIndex, endIndex);
 }
+
+// Fonction pour changer de page
+changePage(pageNumber: number): void {
+  this.currentPage = pageNumber;
+}
+getPageNumbers(): number[] {
+  const pageCount = Math.ceil(this.companies.length / this.companiesPerPage);
+  return Array.from({ length: pageCount }, (_, index) => index + 1);
+}
+ngOnInit(): void {
+
+  this.service.getCompanies().subscribe(
+    (res:any)=>{
+      this.companies=res.result
+      this.companiesFound=this.companies.length
+      console.log(this.companies)
+    },
+    err =>{console.error(err)}
+  )  
+}
+
+
 }
