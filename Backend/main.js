d16c185164463dd47c6b343eb615dddc9049569d
Assil Dkhil <assil.dkhil28@gmail.com>
2024-01-04 20:16:57 +0100

    update mainjs

--- Backend/main.js
+++ Backend/main.js
@@ -1,14 +1,22 @@
+console.log("Aaselemmaaa");
 
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 3800;
 
-app.use(logger('dev'));
-app.use(express.json());
+const cors= require('cors')
 
+app.use(logger('dev'));
+app.use(cookieParser());
+// cros pour que le front end puisse acceder au backEnd (car les deux non pas le meme port) 
+app.use(cors({
+    credentials:true, // pour qu'il puisse acceder au cookie pour s'authentifier
+    origin:['http://localhost:4200']
+})) 
 // bodyParser pour récupérer les données avec post/put
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -18,7 +26,9 @@ app.use(bodyParser.json());
 const job_routes = require('./routers/JobRouter.js');
 app.use('/api/jobs', job_routes);
 
-
+// pour utiliser les routes de user
+const user_routes = require('./routers/UserRouter.js');
+app.use('/api/users', user_routes);
 
 // pour utiliser les routes de company
 const company_routes = require('./routers/CompanyRouter.js');
@@ -27,16 +37,11 @@ app.use('/api/companys', company_routes);
 // pour utiliser les routes de application
 const application_routes = require('./routers/ApplicationRouter.js');
 app.use('/api/applications', application_routes);
-// pour utiliser les routes de user
-
-const user_routers = require('./routers/userRouter');
-app.use('/api/users',user_routers );
 
 // méthode pour se connecter à la base de données avec un fichier env
 require('dotenv').config();
 mongoose.connect(process.env.MONGO_URI)
     .then(() => app.listen(port, () => console.log(`server running on port ${port}`)))
     .catch((error) => console.log('Error', error));
-
 
 
--- Backend/routers/ApplicationRouter.js
+++ Backend/routers/ApplicationRouter.js
@@ -7,12 +7,13 @@ const  {
     getApplications,
     createApplication,
     deleteApplication,
-    doesApplicationExist
+    doesApplicationExist,
+    getApplicationsByJobId
 } = require('../controllers/ApplicationController.js')
 
 router.get('/',getApplications)
 router.post('/',createApplication)
 router.delete('/deleteApplication/:ApplicationID', deleteApplication)
 router.get('/doesApplicationExist/:UserID/:JobID',doesApplicationExist)
-
+router.get('/applicationsByJob/:JobId',getApplicationsByJobId)
 module.exports= router
