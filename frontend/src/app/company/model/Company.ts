7d769c5912200bdf33f7a6ba6de40e82c7224be7
nourhaine1 <ayadinourhaine@gmail.com>
2024-01-09 22:56:44 +0100

    change the logo type in the class

--- frontend/src/app/company/model/Company.ts
+++ frontend/src/app/company/model/Company.ts
@@ -1,9 +1,4 @@
 
-interface Logo {
-    data: Blob; // Using Blob instead of Buffer for frontend representation
-    contentType: string;
-    filename: string;
-  }
 export class Company{
     company_id:number;
     company_name:String;
@@ -12,12 +7,7 @@ export class Company{
 		website:String;
 		email:String;
 		location:String;
-		logo: {
-      data: Blob; // Type for Binary.createFromBase64('...') if needed
-      contentType: string;
-      filename: string;
-      base64: string;
-    };
+		logo:String;
     constructor(
     company_id: number,
     company_name: String,
@@ -26,7 +16,7 @@ export class Company{
     website: String,
     email: String,
     location: String,
-    logo: Blob
+    logo: String
   ) {
     this.company_id = company_id;
     this.company_name = company_name;
@@ -35,12 +25,7 @@ export class Company{
     this.description = description;
     this.secteur = secteur;
     this.website = website;
-    this.logo = {
-      data: logo,
-      contentType: logo.type,
-      filename:'' ,
-      base64:''
-         };
+    this.logo =logo
   }
 
 }
